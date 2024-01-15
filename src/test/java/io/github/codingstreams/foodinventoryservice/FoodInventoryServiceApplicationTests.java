71e08f4b3985436032acb1ac4f92b10304fff23e
Coding Streams <akxhdev@outlook.com>
2024-01-15 12:54:03 +0530

    Initial Commit

---
+++ src/test/java/io/github/codingstreams/foodinventoryservice/FoodInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.codingstreams.foodinventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
