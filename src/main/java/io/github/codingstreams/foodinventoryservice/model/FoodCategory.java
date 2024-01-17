39f1fc111075595a7587e999617aadea9e3e7972
Coding Streams <akxhdev@outlook.com>
2024-01-17 17:04:47 +0530

    MongoDB Relations - END

--- src/main/java/io/github/codingstreams/foodinventoryservice/model/FoodCategory.java
+++ src/main/java/io/github/codingstreams/foodinventoryservice/model/FoodCategory.java
@@ -6,19 +6,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
-@Document(collection = "food-items")
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class FoodItem {
+@Document(collection = "food-categories")
+public class FoodCategory {
   @Id
   private String id;
   private String name;
-  private int quantity;
-
-  @DocumentReference(collection = "food-categories")
-  private FoodCategory foodCategory;
 }
